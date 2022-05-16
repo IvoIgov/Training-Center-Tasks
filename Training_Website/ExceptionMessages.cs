6f9c00cd56fb24869a8051606d0196220387efc6
Ivo Igov <IvoIgov@coherentsolutions.com>
2022-05-16 11:03:30 +0300

    removed unnecessary usings

--- Training_Website/ExceptionMessages.cs
+++ Training_Website/ExceptionMessages.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Training_Website
+﻿namespace Training_Website
 {
     public static class ExceptionMessages
     {
