9a95fa2f688c1cf0e0a3b0d4904551d436ab8de2
Ivo Igov <IvoIgov@coherentsolutions.com>
2022-05-23 12:16:29 +0300

    added fields and properties for Catalog -> ISBN nad Books list

--- Training_Center_Task_3/Catalog.cs
+++ Training_Center_Task_3/Catalog.cs
@@ -8,5 +8,16 @@ namespace Training_Center_Task_3
 {
     public class Catalog
     {
+        private string _isbn;
+        private Dictionary<string, List<Author>> _catalog;
+
+        public string ISBN { get; set; }
+        public Dictionary<string, List<Author>> Authors { get; set; }
+
+        public Catalog(string isbn, List<Author> authors)
+        {
+            this.ISBN = isbn;
+            this.Authors = new Dictionary<string, List<Author>>();
+        }
     }
 }
