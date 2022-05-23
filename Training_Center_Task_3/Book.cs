775ccb1c89097e6cd45812a5258b6eb34d1b0bbe
Ivo Igov <IvoIgov@coherentsolutions.com>
2022-05-23 10:43:37 +0300

    added fields and props for Book - Date and list of uthors

--- Training_Center_Task_3/Book.cs
+++ Training_Center_Task_3/Book.cs
@@ -10,19 +10,9 @@ namespace Training_Center_Task_3
     {
         private string isbn;
         private string title;
-
-        public string Title
-        {
-            get { return title; }
-            private set 
-            { 
-                if (String.IsNullOrEmpty(value))
-                {
-
-                }
-                title = value; }
-        }
-
+        private const int titleLength = 1000;
+        private DateOnly date;
+        private List<Author> authors;
 
         public string ISBN
         {
@@ -30,5 +20,36 @@ namespace Training_Center_Task_3
             private set { isbn = value; }
         }
 
+        public string Title
+        {
+            get { return title; }
+            private set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(ExceptionMessages.TitleCannotBeNull);
+                }
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.TitleCannotBeEmpty);
+                }
+                if (value.Length >= titleLength)
+                {
+                    throw new Exception(ExceptionMessages.TitleTooLong);
+                }
+                title = value;
+            }
+        }
+        public List<Author> Authors
+        {
+            get { return authors; }
+            set { authors = value; }
+        }
+
+        public DateOnly Date
+        {
+            get { return date; }
+            private set { date = value; }
+        }
     }
 }
