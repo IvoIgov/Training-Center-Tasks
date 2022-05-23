23811ebcf5fb21e171a5ada47f4b3488fb058351
Ivo Igov <IvoIgov@coherentsolutions.com>
2022-05-23 10:20:19 +0300

    added exceptions for Book class - Title property. Added a constant for title length, set to 1000 characters

--- Training_Center_Task_3/ExceptionMessages.cs
+++ Training_Center_Task_3/ExceptionMessages.cs
@@ -8,5 +8,8 @@ namespace Training_Center_Task_3
 {
     public class ExceptionMessages
     {
+        public const string TitleCannotBeEmpty = "Title cannot be empty!";
+        public const string TitleCannotBeNull = "Title cannot be null!";
+        public const string TitleTooLong = "Title cannot be more than 1000 characters long!";
     }
 }
