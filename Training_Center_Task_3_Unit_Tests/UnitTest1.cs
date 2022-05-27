a6f1d1c4144269d0bc4bf1f02a769feca4097f83
Ivo Igov <IvoIgov@coherentsolutions.com>
2022-05-27 10:25:59 +0300

    added first unit test

--- Training_Center_Task_3_Unit_Tests/UnitTest1.cs
+++ Training_Center_Task_3_Unit_Tests/UnitTest1.cs
@@ -1,4 +1,7 @@
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Training_Center_Task_3;
 
 namespace Training_Center_Task_3_Unit_Tests
 {
@@ -10,9 +13,19 @@ namespace Training_Center_Task_3_Unit_Tests
         }
 
         [Test]
-        public void Test1()
+        public void Ctor_CreateBookValidData()
         {
-            Assert.Pass();
+            List<Author> authors = new List<Author>();
+            authors.Add(new Author("Ivo", "Igov"));
+            string isbn = "123-4-56-789101-1";
+            string title = "Title 2";
+            DateOnly date = DateOnly.ParseExact("2022-04-23", "yyyy-MM-dd");
+
+            Book book = new Book(isbn, title, date, authors);
+            Assert.AreEqual(book.ISBN, "123-4-56-789101-1");
+            Assert.AreEqual(book.Title, "Title 2");
+            Assert.AreEqual(book.Date, date);
+            Assert.AreEqual(book.Authors.Count, 1);
         }
     }
 }
