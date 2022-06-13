5ac1fc09ce361ad13e005471c90da2d1425c1e15
Ivo Igov <IvoIgov@coherentsolutions.com>
2022-06-13 13:14:11 +0300

    added constants for unit tests

--- Training_Center_Task_4_Tests/UnitTests.cs
+++ Training_Center_Task_4_Tests/UnitTests.cs
@@ -7,16 +7,24 @@ namespace Training_Center_Task_4_Tests
     public class UnitTests
     {
         private User _user;
+        private string _username = "Ivo";
+        private string _mainWindowTitle = "main";
+        private string _validWindowTopValue = "20";
+        private string _validWindowLeftValue = "20";
+        private string _validWindowWidthValue = "20";
+        private string _validWindowHeightValue = "20";
+
+
         [SetUp]
         public void Setup()
         {
-            _user = new User("Ivo");
+            _user = new User(_username);
         }
 
         [Test]
         public void CtorUserValidName()
         {
-            Assert.AreEqual("Ivo", _user.Name);
+            Assert.AreEqual(_username, _user.Name);
         }
 
         [Test]
@@ -28,7 +36,7 @@ namespace Training_Center_Task_4_Tests
         [Test]
         public void CtorWindowAllValidData()
         {
-            Window window = new Window("main", "20", "20", "20", "20");
+            Window window = new Window(_mainWindowTitle, _validWindowTopValue, _validWindowLeftValue, _validWindowWidthValue, _validWindowHeightValue);
             _user.Windows.Add(window);
             Assert.AreEqual(1, _user.Windows.Count);
         }
@@ -36,13 +44,13 @@ namespace Training_Center_Task_4_Tests
         [Test]
         public void CtorWindowEmptyTitle()
         {
-            Assert.Throws<Exception>(() => new Window("", "20", "20", "20", "20"));
+            Assert.Throws<Exception>(() => new Window("", _validWindowTopValue, _validWindowLeftValue, _validWindowWidthValue, _validWindowHeightValue));
         }
 
         [Test]
         public void CtorWindowTopValueEmpty()
         {
-            Window window = new Window("main", "", "20", "20", "20");
+            Window window = new Window(_mainWindowTitle, "", _validWindowLeftValue, _validWindowWidthValue, _validWindowHeightValue);
             _user.Windows.Add(window);
             Assert.AreEqual("?", _user.Windows[0].Top);
         }
@@ -50,13 +58,13 @@ namespace Training_Center_Task_4_Tests
         [Test]
         public void CtorWindowTopValueNegative()
         {
-            Assert.Throws<Exception>(() => new Window("main", "-10", "20", "20", "20"));
+            Assert.Throws<Exception>(() => new Window(_mainWindowTitle, "-10", _validWindowLeftValue, _validWindowWidthValue, _validWindowHeightValue));
         }
 
         [Test]
         public void CtorWindowLeftValueEmpty()
         {
-            Window window = new Window("main", "10", "", "20", "20");
+            Window window = new Window(_mainWindowTitle, _validWindowTopValue, "", _validWindowWidthValue, _validWindowHeightValue);
             _user.Windows.Add(window);
             Assert.AreEqual("?", _user.Windows[0].Left);
         }
@@ -64,13 +72,13 @@ namespace Training_Center_Task_4_Tests
         [Test]
         public void CtorWindowLeftValueNegative()
         {
-            Assert.Throws<Exception>(() => new Window("main", "10", "-20", "20", "20"));
+            Assert.Throws<Exception>(() => new Window(_mainWindowTitle, _validWindowTopValue, "-20", _validWindowWidthValue, _validWindowHeightValue));
         }
 
         [Test]
         public void CtorWindowWidthValueEmpty()
         {
-            Window window = new Window("main", "10", "20", "", "20");
+            Window window = new Window(_mainWindowTitle, _validWindowTopValue, _validWindowLeftValue, "", _validWindowHeightValue);
             _user.Windows.Add(window);
             Assert.AreEqual("?", _user.Windows[0].Width);
         }
@@ -78,13 +86,13 @@ namespace Training_Center_Task_4_Tests
         [Test]
         public void CtorWindowWidthValueNegative()
         {
-            Assert.Throws<Exception>(() => new Window("main", "10", "20", "-20", "20"));
+            Assert.Throws<Exception>(() => new Window(_mainWindowTitle, _validWindowTopValue, _validWindowLeftValue, "-20", _validWindowHeightValue));
         }
 
         [Test]
         public void CtorWindowHeightValueEmpty()
         {
-            Window window = new Window("main", "10", "20", "20", "");
+            Window window = new Window(_mainWindowTitle, _validWindowTopValue, _validWindowLeftValue, _validWindowWidthValue, "");
             _user.Windows.Add(window);
             Assert.AreEqual("?", _user.Windows[0].Height);
         }
@@ -92,7 +100,7 @@ namespace Training_Center_Task_4_Tests
         [Test]
         public void CtorWindowHeightValueNegative()
         {
-            Assert.Throws<Exception>(() => new Window("main", "10", "20", "20", "-20"));
+            Assert.Throws<Exception>(() => new Window(_mainWindowTitle, _validWindowTopValue, _validWindowLeftValue, _validWindowWidthValue, "-20"));
         }
     }
 }
