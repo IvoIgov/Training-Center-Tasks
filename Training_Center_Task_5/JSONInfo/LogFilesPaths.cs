06eed7bc947db960cc2b5ce0f014a626026f30ff
Ivo Igov <IvoIgov@coherentsolutions.com>
2022-06-27 14:06:15 +0300

    removed unnecessary code

--- Training_Center_Task_5/JSONInfo/LogFilesPaths.cs
+++ Training_Center_Task_5/JSONInfo/LogFilesPaths.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Training_Center_Task_5.JSONInfo
+﻿namespace Training_Center_Task_5.JSONInfo
 {
     public class LogFilesPaths
     {
