536667c83132c0ae6eeb9b55264431b71dd6aba2
Ivo Igov <IvoIgov@coherentsolutions.com>
2022-09-29 12:15:12 +0300

    added Home, Login, Inbox pages as classes

---
+++ Training_Center_Task_9/Pages/HomePage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Center_Task_9.Pages
+{
+    public class HomePage : MainPage
+    {
+        private IWebDriver _driver;
+        private const int WebDriverWaitTime = 10;
+        private IWebElement _dgprButton;
+        private IWebElement _mailButton;
+
+        public HomePage(IWebDriver driver) : base(driver)
+        {
+            _driver = driver;
+        }
+
+        public void AcceptGRPR()
+        {
+            _dgprButton = _driver.FindElement(By.CssSelector("button[class='btn primary']"), WebDriverWaitTime);
+            _dgprButton.Click();
+        }
+
+        public LogInPage NavigateToLoginPage()
+        {
+            AcceptGRPR();
+            _mailButton = _driver.FindElement(By.Id("ybarMailLink"), WebDriverWaitTime);
+            _mailButton.Click();
+            return new LogInPage(_driver);
+        }
+    }
+}
