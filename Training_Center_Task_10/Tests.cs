3be4681adc26fc99ff5b57a81d499f165c6febfa
Ivo Igov <IvoIgov@coherentsolutions.com>
2022-10-25 18:19:37 +0300

    completed login test

--- Training_Center_Task_10/Tests.cs
+++ Training_Center_Task_10/Tests.cs
@@ -23,15 +23,34 @@ namespace Training_Center_Task_10
         }
 
         [DataTestMethod]
-        [DataRow("test.tester.10", "testertest10")]
-        [TestMethod]
-        public void Login(string username, string password)
-        {
-            _driver.Navigate().GoToUrl("https://mail.yandex.com/");
+        [DataRow("testtester10", "t3st3rt3st10", "Test")]
+        [DataRow("testtester200", "t3st3rt3st200", "Tester")]
 
-            IWebElement loginButton = _driver.FindElement(By.CssSelector("button[class='Button2 Button2_size_m Button2_view_action Button2_weight_500 Button_Vd8eu21iIVyRdyjGPVfYF PSHeader-NoLoginButton']"), 10);
-            loginButton.Click();
-        
+        [TestMethod]
+        public void Login(string username, string password, string name)
+        {
+            _driver.Navigate().GoToUrl("https://mail.yahoo.com/");
+
+            //Fill in username
+            IWebElement usernameTextbox = _driver.FindElement(By.Id("login-username"), 10);
+            usernameTextbox.SendKeys(username);
+
+            //Click LogIn button
+            IWebElement loginButtonUsername = _driver.FindElement(By.CssSelector("input[id='login-signin']"), 10);
+            loginButtonUsername.Click();
+
+            //Fill in password
+            IWebElement passwordTextbox = _driver.FindElement(By.Id("login-passwd"), 10);
+            passwordTextbox.Click();
+            passwordTextbox.SendKeys(password);
+
+            //Click LogIn button
+            IWebElement loginButtonPassword = _driver.FindElement(By.CssSelector("button[id='login-signin']"), 10);
+            loginButtonPassword.Click();
+
+            IWebElement usernameText = _driver.FindElement(By.CssSelector("span[role='presentation']"), 10);
+
+            Assert.AreEqual(name, usernameText.Text);
         }
     }
 }
