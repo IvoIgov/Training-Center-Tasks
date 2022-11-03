33909206d6a107097a50daec8696c88fe7351789
IvoIgov <ivoigov@gmail.com>
2022-11-03 09:35:56 +0200

    .gitignore file added

---
+++ Training_Center_Task_9/Models/JsonDataPattern.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Training_Center_Task_9.Models
+{
+    public class JsonDataPattern
+    {
+        public string TestName { get; set; }
+        public string User1Username { get; set; }
+        public string User1Password { get; set; }
+    }
+}
